2da2e4b5cd5db90e48cab27b01b9cabbcb7c03f4
georgi-cole <georgi.val.kolev@gmail.com>
2026-03-03 20:39:37 +0200

    Add cinematic credits data.

---
+++ src/data/credits.ts
@@ -0,0 +1,9 @@
+export default [
+  { role: 'Creative Director', name: 'Georgi Cole' },
+  { role: 'Lead Developer', name: 'Georgi Cole' },
+  { role: 'Art & Design', name: 'Georgi Cole' },
+  { role: 'Sound Design', name: 'Georgi Cole' },
+  { role: 'QA & Playtest', name: 'QA Team' },
+  { role: 'Special Thanks', name: 'OpenAI / Copilot' },
+  { role: 'Powered By', name: 'React + TypeScript' },
+];
