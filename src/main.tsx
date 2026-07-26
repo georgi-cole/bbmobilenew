e5c55292d87287e2624c42957199c5e6190d8c14
georgi-cole <georgi.val.kolev@gmail.com>
2026-07-26 14:23:34 +0300

    Load cascade-proof House of Cards bright theme

--- src/main.tsx
+++ src/main.tsx
@@ -19,6 +19,7 @@ import App from './App.tsx'
 import './styles/gameOverResponsiveFixes.css'
 import './styles/houseOfCardsInteractionFix.css'
 import './styles/houseOfCardsBrightTheme.css'
+import './styles/houseOfCardsBrightThemePriority.css'
 
 // Apply html class flags (is-standalone, is-webkit, is-chrome-android) as
 // early as possible so CSS selectors in _ios-standalone-fixes.css and
