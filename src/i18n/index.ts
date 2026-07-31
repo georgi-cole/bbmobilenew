e003bc7bfa78497a9824a8f7f69cb412d3bd4754
georgi-cole <georgi.val.kolev@gmail.com>
2026-07-31 12:44:57 +0300

    Export localization APIs

---
+++ src/i18n/index.ts
@@ -0,0 +1,19 @@
+export { I18nProvider, useI18n } from './I18nProvider'
+export {
+  DEFAULT_APP_LANGUAGE,
+  LANGUAGE_OPTIONS,
+  getLanguageNativeName,
+  getSystemLanguageTags,
+  normalizeLanguagePreference,
+  resolveLanguagePreference,
+  resolveSystemLanguage,
+  type AppLanguage,
+  type LanguagePreference,
+} from './languages'
+export {
+  EN_US_MESSAGES,
+  translate,
+  type Translate,
+  type TranslationKey,
+  type TranslationParams,
+} from './messages'
