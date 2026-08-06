409698ed140470c2baf96f497552e7e0f3639d07
georgi-cole <georgi.val.kolev@gmail.com>
2026-08-06 17:46:32 +0300

    Normalize optional minigame avatar values

--- src/components/ResolvedAvatarImage/ResolvedAvatarImage.tsx
+++ src/components/ResolvedAvatarImage/ResolvedAvatarImage.tsx
@@ -38,7 +38,7 @@ export default function ResolvedAvatarImage({
               resolveAvatarCandidates({
                 id,
                 name,
-                avatar,
+                avatar: avatar ?? '',
                 isUser,
               })
             ),
