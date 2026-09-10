3dfc3d538b30785c9feae7cfc9fc55fd40470e47
georgi-cole <georgi.val.kolev@gmail.com>
2026-09-11 02:11:33 +0300

    Remove temporary formatting diagnostic

--- scripts/check-changed-format.mjs
+++ scripts/check-changed-format.mjs
@@ -3,7 +3,6 @@ import { readFile } from 'node:fs/promises'
 import prettier from 'prettier'
 
 const cwd = process.cwd()
-const formatDiagnosticTarget = 'src/components/Eviction/SpotlightEvictionOverlay.tsx'
 
 function git(args, options = {}) {
   const result = spawnSync('git', args, {
@@ -78,11 +77,6 @@ for (const file of files) {
   const options = { ...config, filepath: file }
   const currentSource = await readFile(file, 'utf8')
   const currentClean = await isPrettierClean(currentSource, options)
-  if (file === formatDiagnosticTarget && !currentClean) {
-    console.log('---BEGIN PRETTIER EVICTION OUTPUT---')
-    console.log(await prettier.format(currentSource, options))
-    console.log('---END PRETTIER EVICTION OUTPUT---')
-  }
   const baseSource = git(['show', `${mergeBase}:${file}`])
 
   if (baseSource == null) {
