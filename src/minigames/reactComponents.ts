17bc9e48f3933a06194852b77e21a7e76d5bbf5b
georgi-cole <georgi.val.kolev@gmail.com>
2026-09-17 11:32:28 +0300

    Load Down Memory Lane polish styles

--- src/minigames/reactComponents.ts
+++ src/minigames/reactComponents.ts
@@ -62,6 +62,7 @@ import BigSpender from '../components/BigSpender/BigSpender'
 import HouseOfDarknessComp from '../components/HouseOfDarknessComp/HouseOfDarknessComp'
 import FinalThreeCircuit from '../components/FinalThreeCircuit/FinalThreeCircuit'
 import DownMemoryLane from '../components/DownMemoryLane/DownMemoryLane'
+import '../components/DownMemoryLane/DownMemoryLanePolish.css'
 
 /**
  * Minimal prop contract shared by all generic React minigame components.
